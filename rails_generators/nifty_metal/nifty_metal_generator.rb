379c461b19e6f20ebec4e1cbe0fbfa65a857f4c4
Tom Wilson <tom@jackhq.com>
2009-05-01 15:21:08 -0400

    added nifty metal

--- rails_generators/nifty_metal/nifty_metal_generator.rb
+++ rails_generators/nifty_metal/nifty_metal_generator.rb
@@ -1,7 +1,7 @@
 class NiftyMetalGenerator < Rails::Generator::Base
   def initialize(runtime_args, runtime_options = {})
     super
-    #@name = @args.first || 'app'
+    #@model_name = @args.first || 'app'
   end
   
   def manifest
